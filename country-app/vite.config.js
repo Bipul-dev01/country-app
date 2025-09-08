91f65a689807adf72de5c2ed6bc9a73fce999466
Tajul <kumar@gmail.com>
2025-09-08 13:23:29 +0600

    Added images folder

--- country-app/vite.config.js
+++ country-app/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/country-app/', 
   server: {
     port: 3001,
   },
 });
+
